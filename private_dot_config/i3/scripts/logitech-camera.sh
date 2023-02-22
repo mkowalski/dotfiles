731be7d33be78c18f9875c99b7c8f91156ba9074
Mat Kowalski <mko@redhat.com>
2023-02-22 10:21:05 +0100

    Update i3 config for camera and reboot

--- private_dot_config/i3/scripts/logitech-camera.sh
+++ private_dot_config/i3/scripts/logitech-camera.sh
@@ -1,4 +1,7 @@
 # Fix webcam focus / exposure
+
+EXTERNAL_CAM=video4
+
 function fixcam_omer {
   v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=0
   v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=0
@@ -6,17 +9,17 @@ function fixcam_omer {
 }
 
 function camera_tune {
-  v4l2-ctl --device=/dev/video4 --set-ctrl zoom_absolute=135
-  v4l2-ctl --device=/dev/video4 --set-ctrl sharpness=150
-  v4l2-ctl --device=/dev/video4 --set-ctrl backlight_compensation=1
-  v4l2-ctl --device=/dev/video4 --set-ctrl tilt_absolute=-36000
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl zoom_absolute=135
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl sharpness=150
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl backlight_compensation=1
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl tilt_absolute=-36000
 }
 
 function camera_reset {
-  v4l2-ctl --device=/dev/video4 --set-ctrl zoom_absolute=100
-  v4l2-ctl --device=/dev/video4 --set-ctrl sharpness=128
-  v4l2-ctl --device=/dev/video4 --set-ctrl backlight_compensation=0
-  v4l2-ctl --device=/dev/video4 --set-ctrl tilt_absolute=0
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl zoom_absolute=100
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl sharpness=128
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl backlight_compensation=0
+  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl tilt_absolute=0
 }
 
 $1
