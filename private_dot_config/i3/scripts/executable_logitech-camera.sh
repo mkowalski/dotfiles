35a06acc49e77775c19981d49115318f5aad1428
Mat Kowalski <mko@redhat.com>
2025-01-29 16:18:01 +0100

    Regular update

--- private_dot_config/i3/scripts/executable_logitech-camera.sh
+++ private_dot_config/i3/scripts/executable_logitech-camera.sh
@@ -1,6 +1,6 @@
 # Fix webcam focus / exposure
 
-EXTERNAL_CAM=video4
+export EXTERNAL_CAM=$(v4l2-ctl --list-devices | grep "Logitech Webcam C925e" -A1 | grep dev | awk '{$1=$1};1')
 
 function fixcam_omer {
   v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=0
@@ -9,24 +9,32 @@ function fixcam_omer {
 }
 
 function camera_tune {
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl zoom_absolute=135
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl sharpness=150
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl backlight_compensation=1
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl tilt_absolute=-36000
+  echo $EXTERNAL_CAM
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl zoom_absolute=135
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl sharpness=150
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl backlight_compensation=1
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl tilt_absolute=-36000
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_automatic=1
+#  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_temperature=6500
 }
 
 function camera_tune2 {
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl zoom_absolute=135
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl sharpness=150
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl backlight_compensation=1
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl tilt_absolute=-10000
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl zoom_absolute=135
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl sharpness=150
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl backlight_compensation=1
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl tilt_absolute=-10000
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_automatic=1
+#  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_temperature=6500
+
 }
 
 function camera_reset {
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl zoom_absolute=100
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl sharpness=128
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl backlight_compensation=0
-  v4l2-ctl --device=/dev/$EXTERNAL_CAM --set-ctrl tilt_absolute=0
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl zoom_absolute=100
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl sharpness=128
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl backlight_compensation=0
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl tilt_absolute=0
+  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_automatic=1
+#  v4l2-ctl --device="$EXTERNAL_CAM" --set-ctrl white_balance_temperature=4000
 }
 
 $1
